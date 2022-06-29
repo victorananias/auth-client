78aa49c27ea819a0e0d5d7e7e377b5342868bad3
Victor <victor@ananias.dev>
2022-06-29 11:34:29 -0300

    wip

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,5 @@
-const baseUrl = 'http://localhost:8080'
-export const login = (username, password) => {
-  const headers = new Headers()
-  headers.append('Content-Type', 'application/json')
-  headers.append('Access-Control-Allow-Origin', '*')
-  headers.append('Access-Control-Allow-Methods', 'DELETE, POST, GET, OPTIONS')
-  headers.append('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
-  const body = {
-    username,
-    password
-  }
-  const init = {
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify(body)
-  }
-  return fetch(`${baseUrl}`, init)
-}
+import axios from 'axios'
+
+const http = axios.create({ baseURL: 'http://localhost:8080', headers: {} })
+
+export const login = async (username, password) => await http.post('login', { username, password })
